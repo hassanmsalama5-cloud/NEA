cc247afc995270b975822474aef2bcee6aa50166
hassanmsalama5-cloud <hassanmsalama5@gmail.com>
2026-03-16 11:29:54 +0000

    Add signup route and update index.html with signup link

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/signup")
+def signup():
+   return "signup page"
+
 @app.route("/", methods=["GET", "POST"])
 def home():
    if request.method == "POST":
       pw = request.form['pw']
-      if pw == "password":
+      if pw == "123":
          return "access granted"
       else:
          return "access denied"
