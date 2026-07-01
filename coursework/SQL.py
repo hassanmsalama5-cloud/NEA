b075902e4a2307e6953be3ad0b878928ff832a23
hassanmsalama5-cloud <hassanmsalama5@gmail.com>
2026-07-01 18:21:02 +0000

    Refactor SQL operations to include film insertion and customer password update

--- coursework/SQL.py
+++ coursework/SQL.py
@@ -2,11 +2,13 @@ import sqlite3
 conn = sqlite3.connect("cinema.db")
 cursor = conn.cursor()
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS Customers (
-    CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
+INSERT INTO films (Title, Genre, Duration, AgeRating, Description) VALUES   (?, ?, ?, ?, ?)          
+""", ('Inception', 'Sci-Fi', 148, 'PG-13', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.'))
+cursor.execute("DELETE FROM films WHERE Title = ?", ('Inception',))
+cursor.execute(
+    "UPDATE Customers SET password = ? WHERE CustomerID = ?",
+    ("123456", 1)
 )
-""")
+
 conn.commit()
 conn.close()
